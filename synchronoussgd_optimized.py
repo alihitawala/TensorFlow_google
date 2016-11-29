957dad10ae615e809be6dbaf07d9d845e7530819
aliHitawala <ali.h.hita@gmail.com>
2016-11-28 19:00:50 -0600

    Optimized version of non parallel implementation

--- synchronoussgd_optimized.py
+++ synchronoussgd_optimized.py
@@ -8,18 +8,12 @@ num_features = 33762578
 g = tf.Graph()
 data_dir = "./data/criteo-tfr-big"
 file_names = [data_dir + '/tfrecords00']
+test_file_names = [data_dir + '/tfrecords22']
 
-with g.as_default():
-      # Define data queue
-    filename_queue = tf.train.string_input_producer(file_names,
-            num_epochs=None)
-
-    # TFRecordReader for data read
+def get_next_row(file_names):
+    filename_queue = tf.train.string_input_producer(file_names, num_epochs=None)
     reader = tf.TFRecordReader()
-
     _, serialized_example = reader.read(filename_queue)
-
-    # Get the features
     features = tf.parse_single_example(serialized_example,
             features={
                 'label' : tf.FixedLenFeature([1], dtype=tf.int64),
@@ -27,10 +21,19 @@ with g.as_default():
                 'value' : tf.VarLenFeature(dtype=tf.float32),
                 }
             )
-
     label = features['label']
     index = features['index']
     value = features['value']
+    return label, index, value
+
+def get_dense_x(index, value):
+    dense_x = tf.sparse_to_dense(tf.sparse_tensor_to_dense(index),
+            [num_features],
+            tf.sparse_tensor_to_dense(value))
+    return tf.reshape(dense_x, [num_features, 1])
+
+with g.as_default():
+    label, index, value = get_next_row(file_names)
 
     # test = tf.gather_nd(params=value, indices=index)
     # Convert to dense feature
@@ -75,7 +78,17 @@ with g.as_default():
     # dense_gradient = tf.reshape(dense_gradient, [num_features, 1])
     # x = tf.IndexedSlices(gradient, tf.sparse_tensor_to_dense(index) ,dense_shape=tf.constant([num_features, 1]))
     # rx = tf.rank(x)
-    update_model = tf.scatter_add(w, tf.sparse_tensor_to_dense(index), tf.reshape(tf.mul(gradient, -0.1), shape=[tf.shape(value)[0], 1]))
+    update_model = tf.scatter_add(w, tf.sparse_tensor_to_dense(index), tf.reshape(tf.mul(gradient, -0.01), shape=[tf.shape(value)[0], 1]))
+
+
+    test_label, test_index, test_value = get_next_row(test_file_names)
+    # test_dense_x = get_dense_x(test_index, test_value)
+    test_w_filtered = tf.gather(w, test_index.values)
+    test_x_filtered = tf.convert_to_tensor(test_value.values, dtype=tf.float32)
+    test_x_filtered = tf.reshape(test_x_filtered, [tf.shape(test_value)[0], 1])
+    sign_actual = tf.cast(tf.sign(tf.matmul(tf.transpose(test_w_filtered), test_x_filtered)[0][0]), tf.int64)
+    sign_expected = tf.sign(test_label[0])
+    sign_values = [sign_actual, sign_expected]
 
     # Create a session
     sess = tf.Session()
@@ -85,7 +98,8 @@ with g.as_default():
     tf.train.start_queue_runners(sess=sess)
 
     # Run n iterations
-    n = 100
+    n = 1000
+    e = 200
     count = 0
     start_total = time.time()
     for i in range(0, n):
@@ -95,4 +109,14 @@ with g.as_default():
         # print (output_g)
         # print (output_g.indices)
         # print (output_g)
+        if i % 100 == 0:
+            start = time.time()
+            count = 0
+            for j in range(0,e):
+                output_sign = sess.run(sign_values)
+                if output_sign[0] != output_sign[1]:
+                    count+=1
+            print "*********Mistakes: " + str(count), str(e) + "**********"
+            # loss_out = sess.run(loss)
+            print "Time in calculating mistakes on test set: " + str(time.time() - start)
     print "Time taken for " + str(n) + " iteration :: " + str(time.time() - start_total)
