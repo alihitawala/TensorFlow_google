19b39e1872780970bb5a37ab509ab6cd313b67b6
Ali Hussain Hitawala <ali.h.hita@gmail.com>
2016-12-02 00:47:37 -0600

    Final version eta=0.1 error_check=200000 iterations=10000*5 for sync dist opt

--- synchronoussgd_distributed_optimized.py
+++ synchronoussgd_distributed_optimized.py
@@ -85,7 +85,7 @@ with g.as_default():
                     ), x_filtered)
             gradients.append(tf.SparseTensor(
                     indices=tf.reshape(index.values, shape=[tf.shape(value)[0], 1]),
-                    values=tf.mul(tf.reshape(local_gradient, tf.shape(value)), -0.01),
+                    values=tf.mul(tf.reshape(local_gradient, tf.shape(value)), -0.1),
                     shape=[num_features]
                 )
             )
