d07d89caec2d9ae30a1cf987875f6ff0eeba623e
Ali Hussain Hitawala <ali.h.hita@gmail.com>
2016-12-05 23:37:02 -0600

    Graphs generated

--- submission/launch_synchronoussgd.sh
+++ submission/launch_synchronoussgd.sh
@@ -8,11 +8,11 @@ source tfdefs_multiple.sh
 start_cluster startserver_multiple.py
 
 echo "Executing the distributed synchronous sgd with multiple client per worker"
-#nohup ssh ubuntu@vm-8-1 "rm ~/output/* ; dstat --output ~/output/sync_stats.csv -cdn" &
-#nohup ssh ubuntu@vm-8-2 "rm ~/output/* ; dstat --output ~/output/sync_stats.csv -cdn" &
-#nohup ssh ubuntu@vm-8-3 "rm ~/output/* ; dstat --output ~/output/sync_stats.csv -cdn" &
-#nohup ssh ubuntu@vm-8-4 "rm ~/output/* ; dstat --output ~/output/sync_stats.csv -cdn" &
-#nohup ssh ubuntu@vm-8-5 "rm ~/output/* ; dstat --output ~/output/sync_stats.csv -cdn" &
+nohup ssh ubuntu@vm-8-1 "rm ~/output/* ; dstat --output ~/output/sync_stats_1.csv -cdn" &
+nohup ssh ubuntu@vm-8-2 "rm ~/output/* ; dstat --output ~/output/sync_stats_2.csv -cdn" &
+nohup ssh ubuntu@vm-8-3 "rm ~/output/* ; dstat --output ~/output/sync_stats_3.csv -cdn" &
+nohup ssh ubuntu@vm-8-4 "rm ~/output/* ; dstat --output ~/output/sync_stats_4.csv -cdn" &
+nohup ssh ubuntu@vm-8-5 "rm ~/output/* ; dstat --output ~/output/sync_stats_5.csv -cdn" &
 
 # testdistributed.py is a client that can run jobs on the cluster.
 # please read testdistributed.py to understand the steps defining a Graph and
